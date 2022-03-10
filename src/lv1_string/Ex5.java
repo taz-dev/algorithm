50aeee421b70acd2194543507657c97c99b17742
taz <tazdev@naver.com>
2022-03-10 17:34:40 +0900

    [20220310] 5. 특정 문자 뒤집기(toCharArray())

---
+++ src/lv1_string/Ex5.java
@@ -0,0 +1,33 @@
+package lv1_string;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public String solution(String str) {
+        String answer = "";
+        char[] arr = str.toCharArray();
+        int lt = 0, rt = str.length()-1;
+
+        while (lt < rt) {
+            if (!Character.isAlphabetic(arr[lt])) lt++;
+            else if (!Character.isAlphabetic(arr[rt])) rt--;
+            else {
+                char tmp = arr[lt];
+                arr[lt] = arr[rt];
+                arr[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        answer = String.valueOf(arr);
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Ex5 T = new Ex5();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+}
