7cebd589aaa963a3ca87304c6ce8293c797e2feb
taz <tazdev@naver.com>
2022-03-10 14:15:58 +0900

    [20220310] 2. 버블 정렬

---
+++ src/lv6_sorting_searching/Ex2.java
@@ -0,0 +1,31 @@
+package lv6_sorting_searching;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Ex2 T = new Ex2();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
