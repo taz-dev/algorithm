da15d8508402f39ee14d8e6b2ab073f317142f5a
taz <tazdev@naver.com>
2022-03-12 23:16:12 +0900

    [20220312] 9. 숫자만 추출

--- src/lv1_string/Ex9.java
+++ src/lv1_string/Ex9.java
@@ -3,13 +3,23 @@ package lv1_string;
 import java.util.Scanner;
 
 public class Ex9 {
-    public int solution(String str){
+    //1 아스키코드
+    //0~9:48~57
+    /*public int solution(String str){
         int answer = 0;
-
         for (char x : str.toCharArray()) {
             if (x >= 48 && x <= 57) answer = answer * 10 + (x - 48);
         }
         return answer;
+    }*/
+
+    //2
+    public int solution(String str){
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isDigit(x)) answer += x;
+        }
+        return Integer.parseInt(answer); //맨 앞에 0이 있을 경우 0을 없애줌
     }
 
     public static void main(String[] args){
