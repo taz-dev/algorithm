13939ee42dfa98d8beffb3e505526bf39761526b
taz <tazdev@naver.com>
2022-03-16 17:46:17 +0900

    [20220316] 6. 뒤집은 소수

---
+++ src/lv2_array/Ex6.java
@@ -0,0 +1,43 @@
+package lv2_array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex6 {
+    public boolean isPrime(int num) {
+        if (num == 1) return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if (isPrime(res)) answer.add(res);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex6 T = new Ex6();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
