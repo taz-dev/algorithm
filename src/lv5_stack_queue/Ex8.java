cecb6dc73df150789b0dc9be50f6de2104f5b977
taz <tazdev@naver.com>
2022-03-29 23:17:19 +0900

    [20220329] 8. 응급실

---
+++ src/lv5_stack_queue/Ex8.java
@@ -0,0 +1,53 @@
+package lv5_stack_queue;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Person{
+    int id;
+    int priority;
+
+    public Person(int id, int priority){
+        this.id = id;
+        this.priority = priority;
+    }
+}
+
+public class Ex8 {
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.offer(new Person(i, arr[i]));
+        }
+        while (!queue.isEmpty()) {
+            Person tmp = queue.poll();
+            for (Person x : queue) {
+                if (x.priority > tmp.priority) {
+                    queue.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) return answer;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
+        Ex8 T = new Ex8();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+    }
+}
