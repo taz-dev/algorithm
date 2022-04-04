2fbf8c9c8c71227337dde72355bd6d3aa97a7992
taz <tazdev@naver.com>
2022-04-04 23:44:30 +0900

    [20220404] 10. 마구간 정하기

---
+++ src/lv6_sorting_searching/Ex10.java
@@ -0,0 +1,44 @@
+package lv6_sorting_searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex10 {
+    public int count(int[] arr, int dist) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
+    }
+
+    public int solution(int n, int c, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n-1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= c) {
+                answer = mid;
+                lt = mid + 1;
+            }
+            else rt = mid - 1;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Ex10 T = new Ex10();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int c = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        System.out.println(T.solution(n, c, arr));
+    }
+}
