6d1bf6f1316243a4599414e054909c499816eccf
taz <tazdev@naver.com>
2022-04-12 23:09:09 +0900

    [20220412] 10. Tree 말단 노드까지의 가장 짧은 경로(BFS)

---
+++ src/lv7_recursive_tree_graph/Ex10.java
@@ -0,0 +1,35 @@
+package lv7_recursive_tree_graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Ex10 {
+    Node root;
+    public int BFS(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                Node current = queue.poll();
+                if (current.lt == null && current.rt == null) return level;
+                if (current.lt != null) queue.offer(current.lt);
+                if (current.rt != null) queue.offer(current.rt);
+            }
+            level++;
+        }
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        Ex10 tree = new Ex10();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        System.out.println(tree.BFS(tree.root));
+    }
+}
