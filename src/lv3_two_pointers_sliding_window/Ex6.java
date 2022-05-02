06baa8f909cfe409a279c8b93a94fccd1c741aa9
taz <tazdev@naver.com>
2022-05-02 23:32:57 +0900

    [20220502] 6. 최대 길이 연속부분 수열

---
+++ src/lv3_two_pointers_sliding_window/Ex6.java
@@ -0,0 +1,32 @@
+package lv3_two_pointers_sliding_window;
+
+import java.util.Scanner;
+
+public class Ex6 {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) cnt++;
+
+            while (cnt > k) {
+                if (arr[lt] == 0) cnt--;
+                lt++;
+            }
+            answer = Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex6 T = new Ex6();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, k, arr));
+    }
+}
