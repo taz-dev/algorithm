3e12bc00214c3bca7fc2d8e5a03b674c8c615fc8
taz <tazdev@naver.com>
2022-05-04 00:02:59 +0900

    [20220504] 7. 교육 과정 설계

--- src/lv5_stack_queue/Ex7.java
+++ src/lv5_stack_queue/Ex7.java
@@ -5,21 +5,23 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Ex7 {
-    public String solution(String need, String plan){
+    public String solution(String need, String plan) {
         String answer = "YES";
-        Queue<Character> queue = new LinkedList<>();
 
-        for(char x : need.toCharArray()) {
-            queue.offer(x);
+        Queue<Character> queue = new LinkedList<>();
+        for (char x : need.toCharArray()) {
+            queue.offer(x); //C B A
         }
-        for(char x : plan.toCharArray()){
-            if(queue.contains(x)){
-                if(x!=queue.poll()) return "NO";
+        for (char x : plan.toCharArray()) {
+            if (queue.contains(x)) { //필수과목일 경우
+                if (x != queue.poll()) return "NO";
+                //queue.poll() : 큐에서 값을 하나 꺼내고 그 값을 리턴받는 메서드
             }
         }
-        if(!queue.isEmpty()) return "NO";
+        if (!queue.isEmpty()) return "NO";
         return answer;
     }
+
     public static void main(String[] args){
         Ex7 T = new Ex7();
         Scanner kb = new Scanner(System.in);
