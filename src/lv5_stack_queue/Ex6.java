b29f507e3520fdd4444e71601506d38cec55d1e2
taz <tazdev@naver.com>
2022-05-03 23:40:01 +0900

    [20220503] 6. 공주 구하기

--- src/lv5_stack_queue/Ex6.java
+++ src/lv5_stack_queue/Ex6.java
@@ -5,19 +5,19 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Ex6 {
-    public int solution(int n, int k){
+    public int solution(int n, int k) {
         int answer = 0;
-        Queue<Integer> queue = new LinkedList<>();
 
-        for(int i = 1; i <= n; i++) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
             queue.offer(i);
         }
-        while(!queue.isEmpty()){
-            for(int i = 1; i < k; i++) {
+        while (!queue.isEmpty()) {
+            for (int i = 1; i < k; i++) {
                 queue.offer(queue.poll());
             }
             queue.poll();
-            if(queue.size() == 1) {
+            if (queue.size() == 1) {
                 answer = queue.poll();
             }
         }
