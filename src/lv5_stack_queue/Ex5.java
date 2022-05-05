21564c6becf0533ee37eeda3f6c72977a197f9dc
taz <tazdev@naver.com>
2022-05-05 23:26:36 +0900

    [20220505] 5. 쇠막대기

--- src/lv5_stack_queue/Ex5.java
+++ src/lv5_stack_queue/Ex5.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Ex5 {
-    public int solution(String str){
-        int cnt=0;
+    public int solution(String str) {
+        int cnt = 0;
         Stack<Character> stack = new Stack<>();
 
-        for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) == '(') stack.push('(');
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
             else{
                 stack.pop();
-                if(str.charAt(i-1) == '(') cnt += stack.size();
+                if (str.charAt(i-1) == '(') cnt += stack.size();
                 else cnt++;
             }
         }
