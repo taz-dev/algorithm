19edd73ff654a82b650618cb5230931ec290a3bc
taz <tazdev@naver.com>
2022-05-10 17:44:54 +0900

    [20220510] 12. 토마토(BFS)

---
+++ src/lv8_dfs_bfs/Ex12.java
@@ -0,0 +1,63 @@
+package lv8_dfs_bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Ex12 {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] tomato, dis;
+    static int m, n;
+    static Queue<Point> queue = new LinkedList<>();
+
+    public void BFS() {
+        while (!queue.isEmpty()) {
+            Point tmp = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = tmp.x + dx[i];
+                int ny = tmp.y + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && tomato[nx][ny] == 0) {
+                    tomato[nx][ny] = 1;
+                    queue.offer(new Point(nx, ny));
+                    dis[nx][ny] = dis[tmp.x][tmp.y] + 1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex12 T = new Ex12();
+        Scanner kb = new Scanner(System.in);
+        m = kb.nextInt();
+        n = kb.nextInt();
+        tomato = new int[n][m];
+        dis = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                tomato[i][j] = kb.nextInt();
+                if (tomato[i][j] == 1) queue.offer(new Point(i, j));
+            }
+        }
+        T.BFS();
+        boolean flag = true;
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tomato[i][j] == 0) flag = false;
+            }
+        }
+        if (flag) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    answer = Math.max(answer, dis[i][j]);
+                }
+            }
+            System.out.println(answer);
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
