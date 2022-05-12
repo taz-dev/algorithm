aef69be1235cfea6da642814bb7c8e3c3ea58c4e
taz <tazdev@naver.com>
2022-05-12 19:05:01 +0900

    [20220512] 15. 피자배달거리(DFS)

---
+++ src/lv8_dfs_bfs/Ex15.java
@@ -0,0 +1,49 @@
+package lv8_dfs_bfs;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex15 {
+    static int n, m, len, answer = Integer.MAX_VALUE;
+    static int[] combi;
+    static ArrayList<Point> hs, pz;
+
+    public void DFS(int level, int s) {
+        if (level == m) {
+            int sum = 0;
+            for (Point h : hs) {
+                int dis = Integer.MAX_VALUE;
+                for (int i : combi) {
+                    dis = Math.min(dis, Math.abs(h.x - pz.get(i).x) + Math.abs(h.y - pz.get(i).y));
+                }
+                sum += dis;
+            }
+            answer = Math.min(answer, sum);
+        } else {
+            for (int i = s; i < len; i++) {
+                combi[level] = i;
+                DFS(level + 1, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex15 T = new Ex15();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        pz = new ArrayList<>();
+        hs = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = kb.nextInt();
+                if (tmp == 1) hs.add(new Point(i, j));
+                else if (tmp == 2) pz.add(new Point(i, j));
+            }
+        }
+        len = pz.size();
+        combi = new int[m];
+        T.DFS(0, 0);
+        System.out.println(answer);
+    }
+}
